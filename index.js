dfb10a3c71463aee82df43bdbbe60d980f58beb4
David Pires <dpires73@yahoo.com>
2020-12-02 21:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
 
-  /*string.toLowerCase() === string
-  return "I can't hear you!"*/
-
+  string.toLowerCase() === string
+  return "I can't hear you!"
+}
+function sayHiToGrandma(string){
+  
   string.toUpperCase() === string
   return "YES INDEED!"
+}
+
+function sayHiToGrandma(string){
 
   string === "I love you, Grandma"
 	return  "I love you, too."
